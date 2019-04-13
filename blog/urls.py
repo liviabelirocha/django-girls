ffd74033eb242e24372cc07893e8ccaa17f7ce24
liviabelirocha <liviabelirocha@outlook.com>
2019-04-13 14:26:17 -0300

    adding views to edit the blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
-    path('post/<int:pk>/', views.post_detail, name='post-detail')
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    path('post/new', views.post_new, name='post-new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post-edit')
 ]
